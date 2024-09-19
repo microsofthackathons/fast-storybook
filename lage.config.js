6f90a0dc53b63f09db38760661cab430c878536f
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-09-19 14:19:10 -0700

    add lage

---
+++ lage.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    pipeline: {
+        build: {
+            outputs: ["dist/**"],
+            dependsOn: ["^build"],
+        },
+        "build:storybook": {
+            outputs: ["storybook-static/**"],
+            dependsOn: ["^build"],
+        },
+    },
+};
